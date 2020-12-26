4edb2e0da06b9f37acd595024c532b6e0806f00e
Habib Rahman <contact@devhabib.com>
2020-12-27 04:35:55 +0600

    about page is completed and router problem is resolved

--- src/app/modules/main-view/homepage/homepage.module.ts
+++ src/app/modules/main-view/homepage/homepage.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as components from './index';
+
 import { HomepageRoutingModule } from './homepage-routing.module';
-import { HomepageComponent } from './homepage.component';
-import { HeroSliderComponent } from './hero-slider/hero-slider.component';
 import { SwiperModule } from 'ngx-swiper-wrapper';
 import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
-import { TestimonialSliderComponent } from './testimonial-slider/testimonial-slider.component';
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
   slidesPerView: 'auto'
 };
 @NgModule({
-  declarations: [HomepageComponent, HeroSliderComponent, TestimonialSliderComponent],
+  declarations: [components.component],
   imports: [
     CommonModule,
     HomepageRoutingModule,
---
+++ src/app/modules/main-view/homepage/index.ts
@@ -0,0 +1,9 @@
+import { HeroSliderComponent } from "./hero-slider/hero-slider.component";
+import { HomepageComponent } from "./homepage.component";
+import { TestimonialSliderComponent } from "./testimonial-slider/testimonial-slider.component";
+
+export const component: any[] = [
+  HomepageComponent,
+  HeroSliderComponent,
+  TestimonialSliderComponent
+]
